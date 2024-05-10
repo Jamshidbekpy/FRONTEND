69933cb0bffaac16f5b11719b9bd1a099713413e
Jamshidbek Shodibek <jamshidbekshodibek@Jamshidbeks-MacBook-Pro.local>
2024-05-10 23:00:40 +0500

    Page1 added

---
+++ Page1/javascript/main.js
@@ -0,0 +1,9 @@
+function moveToSecondForm() {
+    document.getElementById('firstForm').style.display = 'none';
+    document.getElementById('secondForm').style.display = 'block';
+}
+
+function moveToFirstForm() {
+    document.getElementById('secondForm').style.display = 'none';
+    document.getElementById('firstForm').style.display = 'block';
+}
